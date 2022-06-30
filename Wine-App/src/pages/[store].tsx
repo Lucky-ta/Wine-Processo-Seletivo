8f130e5f3566b912ef31ce060c1f7de8a4ab7dfb
Lukita <lucmaieski@gmail.com>
2022-06-30 18:28:26 -0300

    Returns with pagination implementation

--- Wine-App/src/pages/[store].tsx
+++ Wine-App/src/pages/[store].tsx
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
+import axios from 'axios';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Pagination from '../components/Pagination';
@@ -7,8 +9,8 @@ import ProductCards from '../components/ProductCards';
 import SearchLeftBar from '../components/SearchLeftBar';
 import MyContext from '../contexts/MyContext';
 
-function Store() {
-  const { products, setProducts, filteredProducts } = useContext<any>(MyContext);
+function Store({ items }) {
+  const { products, filteredProducts } = useContext<any>(MyContext);
 
   return (
     <div>
@@ -16,10 +18,27 @@ function Store() {
       <SearchLeftBar />
       {filteredProducts !== null
         ? filteredProducts.map((i) => <ProductCards key={i.id} item={i} />)
-        : products.map((i) => <ProductCards key={i.id} item={i} />)}
+        : items.map((i) => <ProductCards key={i.id} item={i} />)}
       <Pagination />
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = [1, 2, 3, 4, 5, 6, 7];
+  const paths = data.map((p) => ({ params: { store: JSON.stringify(p) } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { store } = context.params;
+  const response = await axios.get(
+    `https://wine-back-test.herokuapp.com/products?page=${store}&limit=10`,
+  );
+  const json = response.data;
+  return { props: json, revalidate: 3600 };
+};
 
 export default Store;
