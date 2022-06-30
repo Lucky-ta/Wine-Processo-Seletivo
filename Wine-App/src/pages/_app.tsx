8bbe15ac05ab103608b0a03947e32a9f3abdae16
Lukita <lucmaieski@gmail.com>
2022-06-29 23:40:04 -0300

    Add MyContext in _app

--- Wine-App/src/pages/_app.tsx
+++ Wine-App/src/pages/_app.tsx
@@ -4,13 +4,14 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../../styles/global';
+import MyProvider from '../contexts/MyProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <MyProvider>
       <Component {...pageProps} />
       <GlobalStyle />
-    </>
+    </MyProvider>
   );
 }
 
