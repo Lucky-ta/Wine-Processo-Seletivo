edfed90fa31adff1dbfad764dba4fea1abe64db1
Lukita <lucmaieski@gmail.com>
2022-06-29 23:58:07 -0300

    Remove fetch from store page

--- Wine-App/src/components/SearchLeftBar.tsx
+++ Wine-App/src/components/SearchLeftBar.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React, { useContext } from 'react';
+import MyContext from '../contexts/MyContext';
 
 function SearchLeftBar() {
+  const { products, setProducts } = useContext<any>(MyContext);
+
+  const handleFilterClick = (value) => {
+    console.log(value);
+
+    const filteredProducts = products.filter((p) => p.price <= value);
+    setProducts(filteredProducts);
+    console.log('Products: ', products);
+  };
   return (
     <section>
       <strong>Refine sua busca</strong>
       <p>Por preço</p>
       <form>
         <label htmlFor="40">
-          <input type="radio" id="40" />
+          <input
+            onClick={(e: any) => handleFilterClick(e.target.value)}
+            value={40}
+            type="radio"
+            id="40"
+          />
           Até R$40
         </label>
         <label htmlFor="40-60">
--- Wine-App/src/pages/[store].tsx
+++ Wine-App/src/pages/[store].tsx
@@ -1,15 +1,16 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
-import axios from 'axios';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Pagination from '../components/Pagination';
 import ProductCards from '../components/ProductCards';
 import SearchLeftBar from '../components/SearchLeftBar';
 import { IApiResponse } from '../interfaces/IApiResponse';
+import MyContext from '../contexts/MyContext';
 
 function Store({ items }: IApiResponse) {
+  const { products, setProducts } = useContext<any>(MyContext);
+
   return (
     <div>
       <Header />
@@ -21,25 +22,5 @@ function Store({ items }: IApiResponse) {
     </div>
   );
 }
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const data = [1, 2, 3, 4, 5, 6, 7];
-  const paths = data.map((p) => ({ params: { store: JSON.stringify(p) } }));
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { store } = context.params;
-
-  const response = await axios.get(
-    `https://wine-back-test.herokuapp.com/products?page=${store}&limit=10`,
-  );
-
-  const json = response.data;
-  return { props: json, revalidate: 3600 };
-};
 
 export default Store;
