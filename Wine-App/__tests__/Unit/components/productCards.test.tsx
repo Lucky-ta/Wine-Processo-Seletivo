6a18ca07f5a3903892d2a83b043fe47a906f6173
Lukita <lucmaieski@gmail.com>
2022-07-01 04:21:50 -0300

    Implements first test for Product Card

---
+++ Wine-App/__tests__/Unit/components/productCards.test.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import ProductCards from '../../../src/components/ProductCards';
+import mockItem from './mocks/mockItems.json';
+
+describe('Test Product Cards Component', () => {
+  it('Must have the follows tags: 1 img, 1 heading, 3 paragraphs.', () => {
+    render(<ProductCards item={mockItem} />);
+
+    const imageComponent = screen.getByRole('img');
+    const h2Component = screen.getByRole('heading');
+    const firstParagraph = screen.getByText(`${mockItem.price}, ${mockItem.discount}`);
+    const secondParagraph = screen.getByText(`SÓCIO WINE R$${mockItem.priceMember}`);
+    const thirdParagraph = screen.getByText(`Nao socio, ${mockItem.price}`);
+
+    expect(imageComponent).toBeInTheDocument();
+    expect(h2Component).toBeInTheDocument();
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+    expect(thirdParagraph).toBeInTheDocument();
+  });
+});
