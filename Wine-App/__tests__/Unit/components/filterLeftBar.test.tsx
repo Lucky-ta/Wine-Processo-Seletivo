9d6152f4808fd7e0d3a5dba75a48b570452458b7
Lukita <lucmaieski@gmail.com>
2022-07-01 03:59:51 -0300

    Implements first test for left filter component

---
+++ Wine-App/__tests__/Unit/components/filterLeftBar.test.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import SearchLeftBar from '../../../src/components/SearchLeftBar';
+
+describe('Test Left Filter Component', () => {
+  it('Must have 5 radio buttons on screen', () => {
+    render(<SearchLeftBar />);
+
+    const radioButtons = screen.getAllByRole('radio');
+    expect(radioButtons.length).toBe(5);
+  });
+});
