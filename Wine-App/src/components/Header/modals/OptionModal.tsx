09e15bbc697cd5bda9197af2ae0f6b94c3286305
Lukita <lucmaieski@gmail.com>
2022-07-02 19:02:30 -0300

    Set initial css for menu options

--- Wine-App/src/components/Header/modals/OptionModal.tsx
+++ Wine-App/src/components/Header/modals/OptionModal.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { VscAccount } from 'react-icons/vsc';
 import Container from '.';
+import { Button } from '..';
 
 function OptionModal() {
   return (
     <Container>
-      <h1>MODAL</h1>
+      <Button
+        data-testid="profileButton"
+        className="menuProfilePic"
+        type="button"
+      >
+        <VscAccount size="2.8em" />
+        Lucas Maieski - (WINE MEMBER)
+      </Button>
     </Container>
   );
 }
--- Wine-App/src/components/Header/modals/index.ts
+++ Wine-App/src/components/Header/modals/index.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     position: fixed;
-    background-color: grey;
+    background-color: white;
+    /* border-right: solid 1px; */
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
     width: 100%;
     max-width: 20em;
     height: 90%;
@@ -10,17 +12,28 @@ const Container = styled.div`
     bottom: 0;
     animation-duration: 0.8s;
     animation-name: slidein;
+
   @keyframes slidein {
   from {
     margin-left: -45%;
-    /* width: 300%; */
   }
 
   to {
     margin-left: 0%;
     width: 100%;
   }
+
 }
+
+    .menuProfilePic {
+        margin-top: 2em;
+        margin-left: 2em;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        gap: 1em;
+    }
+
 `;
 
 export default Container;
