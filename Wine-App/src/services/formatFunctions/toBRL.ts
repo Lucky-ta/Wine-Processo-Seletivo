72854ea9353fdf5578e918a51ad6850b6fc50dad
Lukita <lucmaieski@gmail.com>
2022-07-03 17:31:32 -0300

    Delete formatBRL functio

--- Wine-App/src/services/formatFunctions/toBRL.ts
+++
@@ -1,6 +0,0 @@
-const convertToBRL = (value: number) => {
-  const result = value.toFixed(2).replace(/(\d{1,3}|\G\d{3})(?=(?:\d{3})+(?!\d))/g, '$1,');
-  return result;
-};
-
-export default convertToBRL;
