30cc45704e94902213293cfcfb971fc64e5dae01
Lukita <lucmaieski@gmail.com>
2022-07-04 02:59:01 -0300

    Fix GrayBackground height

--- Wine-App/src/components/GreyBackground/index.ts
+++ Wine-App/src/components/GreyBackground/index.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
     position: fixed;
     width: 100%;
-    height: 100rem;
+    height: 999%;
     background-color: grey;
     opacity: 0.5;
     z-index: -1;
